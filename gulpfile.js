372394dd8fee675f9af1e50d018f5e269d0dc2c1
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-01-13 13:45:05 -0200

    Css adjusts; add gulp vinyl frp for deploy;

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const ftp = require('vinyl-ftp')
+
+let user = process.env.user
+let password = process.env.pass
+let host = 'ftp.gustavocardoso.me'
+let port = 21
+let localFilesGlob = ['./**/*', '!node_modules/**']
+let remoteFolder = 'www/sandbox/flexbox-layout'
+
+console.log(user)
+
+const conn = () => {
+  return ftp.create({
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    parallel: 5,
+    reload: true,
+    log: gutil.log
+  })
+}
+
+gulp.task('ftp-deploy', () => {
+  let connection = conn()
+  
+  return gulp.src(localFilesGlob, { base: '.', buffer: false })
+  .pipe(connection.newer(remoteFolder))
+  .pipe(connection.dest(remoteFolder))
+})
